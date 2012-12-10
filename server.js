3797143f644c3b104569013dee6b03d425400980
secretrobotron <secretrobotron@gmail.com>
2012-12-10 11:08:23 -0500

    stylus and server bits

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 var express = require('express'),
     config = require('config'),
+    stylus = require('stylus'),
     app = express();
 
-app.use(express.logger())
-   .use(express.static(__dirname + '/public'));
+app
+    .use(express.logger('dev'))
+    .use(stylus.middleware({
+      src: __dirname + '/src',
+      dest: __dirname + '/public',
+      compile: function(str, path){
+        return stylus(str)
+          .set('filename', path)
+          .set('warn', true)
+          .set('compress', true);
+      }
+    }))
+    .use(express.static(__dirname + '/public'));
 
 app.listen(config.port);
