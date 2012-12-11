f63f7371ae52012df724e04532e0f2025196d81d
secretrobotron <secretrobotron@gmail.com>
2012-12-11 15:25:09 -0500

    url style fix

--- public/src/snippets/url.js
+++ public/src/snippets/url.js
@@ -1,15 +1,22 @@
 define([], function(){
   
   return function(input){
-    var div;
+    var div, innerDiv;
     if(input.indexOf('.') > -1 && input.indexOf(' ') === -1){
+      innerDiv = document.createElement('div');
+
       div = document.createElement('div');
       div.id = 'url-' + Date.now();
       div.classList.add('url');
+
       var a = document.createElement('a');
       a.href = input;
       a.appendChild(document.createTextNode(input));
-      div.appendChild(a);
+
+      innerDiv.classList.add( "inner" );
+      innerDiv.appendChild(a);
+
+      div.appendChild( innerDiv );
 
       return {
         element: div
